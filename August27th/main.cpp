b3f5c1cd96b0c3bbae76443e27aa0bdc2780a34e
A00630970 <A00630970@69THF24>
2026-08-27 10:55:07 -0500

    Add project files.

---
+++ August27th/main.cpp
@@ -0,0 +1,34 @@
+//The pink pony club kareoke program
+
+#include <iostream>
+
+#include <windows.h> //there is "function" that allows for a "pause" capability
+
+
+using namespace std;
+
+int main()
+{
+
+	//print the lyrics with appropriate time delays 
+	cout << "I";
+	int time = 1000; //an integer is a "data type" (a type of data)
+	float PI = 3.14;
+
+	//another example of an EXTREMELY commonly-occuring data type
+	Sleep(1'000); //unit ,illiseconds(ms)-> not seconds or hours etc 
+	cout << "know";
+
+
+	time = 5'000;//update variable's value to 5 seconds
+	
+
+	Sleep(time);// this is a "function call"
+	cout << "you";
+
+	Sleep(3'000);
+	cout << "wanted";
+}
+
+
+	
